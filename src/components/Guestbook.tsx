718117b63e75a7fbb0838574b86a326bcdd56819
mishalzubair <160513180+mishal230@users.noreply.github.com>
2026-09-18 18:21:41 +0500

    updated

--- src/components/Guestbook.tsx
+++ src/components/Guestbook.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { X } from "lucide-react";
 import { supabase } from "@/integrations/supabase/client";
 
-const CURRENT_PROJECT_ID = "red and white"; // Change this string for your next client
+const CURRENT_PROJECT_ID = "neon golden and white"; // Change this string for your next client
 type Message = {
   id: string;
   name: string;
